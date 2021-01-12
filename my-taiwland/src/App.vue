a3bdd30c2f915524bcbcdab537f1857c61c5ca87
jjunieko <juniorgtr123@gmail.com>
2021-01-12 12:56:07 -0300

    first commit

---
+++ my-taiwland/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <HelloWorld msg="Teste em vue" />
+</template>
+
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Addnew from'./components/Addnew.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Addnew,
+    HelloWorld
+    
+  }
+}
+</script>
